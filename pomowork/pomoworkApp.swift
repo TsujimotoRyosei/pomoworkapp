d1f54cfaae57055751104de5eb763baf7f79d7e3
ryosei15 <c1107582@planet.kanazawa-it.ac.jp>
2024-02-18 12:27:40 +0900

    Initial Commit

---
+++ pomowork/pomoworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomoworkApp.swift
+//  pomowork
+//
+//  Created by ryosei on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct pomoworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
